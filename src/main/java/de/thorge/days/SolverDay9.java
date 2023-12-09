e1bdcfb82dc9622c3447db1ebe5e260a010770ae
TeddyBear2004 <56771258+TeddyBear2004@users.noreply.github.com>
2023-12-09 10:34:51 +0100

    add day 9

--- src/main/java/de/thorge/days/SolverDay9.java
+++ src/main/java/de/thorge/days/SolverDay9.java
@@ -3,37 +3,128 @@ package de.thorge.days;
 import de.thorge.solver.RiddleSolver;
 import de.thorge.solver.Solver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SolverDay9 extends Solver {
 
-	@Override
-	public RiddleSolver<Integer> getFirstRiddleSolver() {
-		return new RiddleSolver<>() {
-			@Override
-			public Integer getSolution() {
-				return null;
-			}
+    @Override
+    public RiddleSolver<Integer> getFirstRiddleSolver() {
+        return new RiddleSolver<>() {
+            @Override
+            public Integer getSolution() {
+                return 114;
+            }
 
-			@Override
-			public Integer solveRiddle() {
-				return null;
-			}
-		};
-	}
+            @Override
+            public Integer solveRiddle() {
+                return splitLinesAndWords()
+                        .map(strings -> Arrays.stream(strings)
+                                .filter(s -> {
+                                    try{
+                                        Integer.parseInt(s);
+                                        return true;
+                                    }catch(NumberFormatException e){
+                                        return false;
+                                    }
+                                })
+                                .map(Integer::parseInt).toList())
+                        .map(integers -> {
+                            List<List<Integer>> integers1 = new ArrayList<>();
+                            integers1.add(new ArrayList<>(integers));
+                            return integers1;
+                        })
+                        .map(integers -> getNextValue(integers))
+                        .intSum();
+            }
+        };
+    }
 
-	@Override
-	public RiddleSolver<Integer> getSecondRiddleSolver() {
-		return new RiddleSolver<>() {
-			@Override
-			public Integer getSolution() {
-				return null;
-			}
+    public int getNextValue(List<List<Integer>> integers) {
+        int lastLayer = integers.size() - 1;
+        List<Integer> lastLayerInts = integers.get(lastLayer);
 
-			@Override
-			public Integer solveRiddle() {
-				return null;
-			}
-		};
-	}
+        if (lastLayerInts.stream().allMatch(integer -> integer == 0)) {
+            lastLayerInts.add(0);
+
+            for (int i = lastLayer; i >= 1; i--) {
+                int lastElement = integers.get(i).size() - 1;
+                int a = integers.get(i).get(lastElement) + integers.get(i - 1).get(lastElement);
+                integers.get(i - 1).add(a);
+            }
+
+            return integers.get(0).get(integers.get(0).size() - 1);
+        }
+        List<Integer> integers1 = new ArrayList<>();
+        for (int i = 1; i < lastLayerInts.size(); i++) {
+            integers1.add(lastLayerInts.get(i) - lastLayerInts.get(i - 1));
+        }
+
+        integers.add(integers1);
+        return getNextValue(integers);
+    }
+
+    @Override
+    public RiddleSolver<Integer> getSecondRiddleSolver() {
+        return new RiddleSolver<>() {
+            @Override
+            public Integer getSolution() {
+                return 2;
+            }
+
+            @Override
+            public Integer solveRiddle() {
+                return splitLinesAndWords()
+                        .map(strings -> Arrays.stream(strings)
+                                .filter(s -> {
+                                    try{
+                                        Integer.parseInt(s);
+                                        return true;
+                                    }catch(NumberFormatException e){
+                                        return false;
+                                    }
+                                })
+                                .map(Integer::parseInt).toList())
+                        .map(integers -> {
+                            List<List<Integer>> integers1 = new ArrayList<>();
+                            integers1.add(new ArrayList<>(integers));
+                            return integers1;
+                        })
+                        .map(integers -> getPreviousValue(integers))
+                        .intSum();
+            }
+        };
+    }
+
+    public int getPreviousValue(List<List<Integer>> integers) {
+        int lastLayer = integers.size() - 1;
+        List<Integer> lastLayerInts = integers.get(lastLayer);
+
+        if (lastLayerInts.stream().allMatch(integer -> integer == 0)) {
+            lastLayerInts.add(0, 0);
+
+            for (int i = lastLayer; i >= 1; i--) {
+                int firstElement = 0;
+                int a = integers.get(i - 1).get(firstElement) - integers.get(i).get(firstElement);
+                integers.get(i - 1).add(0, a);
+            }
+
+            for (List<Integer> integer : integers) {
+                System.out.println(integer);
+            }
+
+            return integers.get(0).get(0);
+        }
+        List<Integer> integers1 = new ArrayList<>();
+        for (int i = 1; i < lastLayerInts.size(); i++) {
+            integers1.add(lastLayerInts.get(i) - lastLayerInts.get(i - 1));
+        }
+
+        integers.add(integers1);
+        return getPreviousValue(integers);
+    }
+
 }
 
 
