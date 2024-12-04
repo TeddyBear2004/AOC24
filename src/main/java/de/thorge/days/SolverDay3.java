fb4f06f7853979cc9760983d44109cefb53fb8cc
Thorge Engelmann <56771258+TeddyBear2004@users.noreply.github.com.>
2024-12-04 13:21:52 +0100

    Added day 4 solution

--- src/main/java/de/thorge/days/SolverDay3.java
+++ src/main/java/de/thorge/days/SolverDay3.java
@@ -1,15 +1,10 @@
 package de.thorge.days;
 
-import de.thorge.AdvancedList;
 import de.thorge.solver.RiddleSolver;
 import de.thorge.solver.Solver;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SolverDay3 extends Solver {
 
@@ -18,85 +13,12 @@ public class SolverDay3 extends Solver {
         return new RiddleSolver<>() {
             @Override
             public Long getSolution() {
-                return 4361L;
+                return 0L;
             }
 
             @Override
             public Long solveRiddle() {
-                AdvancedList<AdvancedList<Character>> advancedLists = splitLinesAndConvert(s -> {
-                    AdvancedList<Character> objects = new AdvancedList<>();
-                    for (char c : s.toCharArray()) {
-                        objects.add(c == '.' ? null : c);
-                    }
-                    objects.add(null);
-
-                    return objects;
-                });
-
-                long sum = 0;
-                for (int lineNr = 0; lineNr < advancedLists.size(); lineNr++) {
-                    int currentInt = 0;
-                    AtomicBoolean isConfirmed = new AtomicBoolean(false);
-
-                    nextChar:
-                    for (int charNr = 0; charNr < advancedLists.get(lineNr).size(); charNr++) {
-                        Character c = advancedLists.get(lineNr).get(charNr);
-
-                        if (c != null) {
-                            if (Character.isDigit(c)) {
-                                currentInt = currentInt * 10 + Character.getNumericValue(c);
-                            } else {
-                                if (currentInt != 0) {
-                                    if (isConfirmed.get()) {
-                                        sum += currentInt;
-                                    }
-
-                                }
-
-                                isConfirmed.set(false);
-                                currentInt = 0;
-                                continue;
-                            }
-                        } else {
-                            if (currentInt != 0) {
-                                if (isConfirmed.get()) {
-                                    sum += currentInt;
-                                }
-
-                            }
-
-                            isConfirmed.set(false);
-                            currentInt = 0;
-                            continue;
-                        }
-
-                        for (int i = -1; i < 2; i++) {
-                            for (int j = -1; j < 2; j++) {
-                                if (i == 0 && j == 0)
-                                    continue;
-
-                                if (!Character.isDigit(c)) {
-                                    continue nextChar;
-                                }
-
-                                if (isConfirmed.get())
-                                    continue;
-
-                                int dx = lineNr + i;
-                                int dy = charNr + j;
-
-                                advancedLists.getOptional(dx)
-                                        .flatMap(characters -> characters.getOptional(dy))
-                                        .filter(character -> !Character.isSpaceChar(character))
-                                        .filter(character -> character != '\r')
-                                        .filter(character -> !Character.isDigit(character))
-                                        .ifPresent(character -> isConfirmed.set(true));
-                            }
-                        }
-                    }
-
-                }
-                return sum;
+                return 0L;
             }
         };
     }
@@ -106,85 +28,45 @@ public class SolverDay3 extends Solver {
         return new RiddleSolver<>() {
             @Override
             public Long getSolution() {
-                return 467835L;
+                return 106921067L;
             }
 
             @Override
             public Long solveRiddle() {
-                AdvancedList<AdvancedList<Character>> advancedLists = splitLinesAndConvert(s -> {
-                    AdvancedList<Character> objects = new AdvancedList<>();
-                    for (char c : s.toCharArray()) {
-                        objects.add(c == '.' ? null : c);
-                    }
-                    objects.add(null);
-
-                    return objects;
-                });
-
-                Map<Integer, Set<AtomicInteger>> map = new HashMap<>();
-
-                for (int lineNr = 0; lineNr < advancedLists.size(); lineNr++) {
-
-                    AtomicInteger currentInt = new AtomicInteger();
-                    nextChar:
-                    for (int charNr = 0; charNr < advancedLists.get(lineNr).size(); charNr++) {
-                        Character c = advancedLists.get(lineNr).get(charNr);
-
-                        if (c != null) {
-                            if (Character.isDigit(c)) {
-                                currentInt.set(currentInt.get() * 10 + Character.getNumericValue(c));
-                            } else {
-                                currentInt = new AtomicInteger();
-                                continue;
-                            }
-                        } else {
-                            currentInt = new AtomicInteger();
+                Pattern compile = Pattern.compile("mul\\((\\d{1,3}),(\\d{1,3})\\)");
+                return splitLines().map(s -> {
+                    String[] segments = s.split("(?=do\\(\\))|(?=don't\\(\\))");
+                    long solution = 0;
+                    for (String segment : segments) {
+                        if (segment.startsWith("don't()")) {
                             continue;
                         }
 
-                        for (int i = -1; i < 2; i++) {
-                            for (int j = -1; j < 2; j++) {
-                                if (i == 0 && j == 0)
-                                    continue;
-
-                                if (!Character.isDigit(c)) {
-                                    continue nextChar;
-                                }
-
-                                int dx = lineNr + i;
-                                int dy = charNr + j;
-
-                                AtomicInteger finalCurrentInt = currentInt;
-                                advancedLists.getOptional(dx)
-                                        .flatMap(characters -> characters.getOptional(dy))
-                                        .filter(character -> character == '*')
-                                        .ifPresent(character -> {
-                                            int i1 = (dx * 1000) + dy;
-                                            map.putIfAbsent(i1, new HashSet<>());
-                                            map.get(i1).add(finalCurrentInt);
-                                        });
-                            }
+                        Matcher matcher = compile.matcher(segment);
+                        while (matcher.find()) {
+                            int prod = getProd(matcher);
+                            solution += prod;
                         }
+
                     }
-                }
+                    return solution;
 
-                long sum = 0;
-
-                for (Set<AtomicInteger> value : map.values()) {
-                    if (value.size() == 2) {
-                        int m = 1;
-                        for (AtomicInteger atomicInteger : value) {
-                            m *= atomicInteger.get();
-                        }
-                        sum += m;
-                    }
-                }
-
-                return sum;
+                }).longSum();
             }
         };
     }
 
+    private static int getProd(Matcher matcher) {
+        int prod = 1;
+        for (int i1 = 1; i1 <= matcher.groupCount(); i1++) {
+            String group = matcher.group(i1);
+            if (group != null) {
+                prod *= Integer.parseInt(group);
+            }
+        }
+
+        return prod;
+    }
 }
 
 
--- src/main/java/de/thorge/days/SolverDay4.java
+++ src/main/java/de/thorge/days/SolverDay4.java
@@ -1,39 +1,261 @@
 package de.thorge.days;
 
+import de.thorge.AdvancedList;
 import de.thorge.solver.RiddleSolver;
 import de.thorge.solver.Solver;
 
-public class SolverDay4 extends Solver {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+public class SolverDay4 extends Solver {
+	private static final char[] WORD = new char[]{'X', 'M', 'A', 'S'};
 	@Override
 	public RiddleSolver<Integer> getFirstRiddleSolver() {
 		return new RiddleSolver<>() {
 			@Override
 			public Integer getSolution() {
-				return null;
+				return 18;
 			}
 
 			@Override
 			public Integer solveRiddle() {
-				return null;
+				AdvancedList<char[]> strings = splitLinesAndConvert(String::toCharArray);
+				int sum = 0;
+
+				for (char[] strings1 : strings) {
+					sum += searchWord(strings1);
+				}
+
+				for (int i = 0; i < strings.get(0).length; i++) {
+					char[] chars = new char[strings.size()];
+					for (int j = 0; j < strings.size(); j++) {
+						chars[j] = strings.get(j)[i];
+					}
+
+					sum += searchWord(chars);
+				}
+
+				for (int i = 0; i < strings.size(); i++) {
+					int j = 0;
+					List<Character> chars = new ArrayList<>();
+
+					for (int di = 0; di < strings.size() - i; di++) {
+						chars.add(strings.get(i + di)[j + di]);
+					}
+
+					sum += searchWord(toArr(chars));
+				}
+
+				for (int j = 1; j < strings.get(0).length; j++) {
+					int i = 0;
+					List<Character> chars = new ArrayList<>();
+
+					for (int di = 0; di < strings.get(0).length - j; di++) {
+						chars.add(strings.get(i + di)[j + di]);
+					}
+
+					sum += searchWord(toArr(chars));
+				}
+
+
+				for (int i = strings.size() - 1; i >= 0; i--) {
+					List<Character> chars = new ArrayList<>();
+
+					for (int di = 0; di <= i; di++) {
+						chars.add(strings.get(i - di)[di]);
+					}
+
+					sum += searchWord(toArr(chars));
+				}
+
+				for (int j = 1; j < strings.get(0).length; j++) {
+					List<Character> chars = new ArrayList<>();
+
+					for (int di = 0; di < strings.get(0).length - j; di++) {
+						chars.add(strings.get(strings.size() - 1 - di)[j + di]);
+					}
+
+					sum += searchWord(toArr(chars));
+				}
+
+				return sum;
 			}
 		};
 	}
+
+	private static char[] toArr(List<Character> chars) {
+		char[] arr = new char[chars.size()];
+		for (int i = 0; i < chars.size(); i++) {
+			arr[i] = chars.get(i);
+		}
+		return arr;
+	}
+
+	private static int[][] toArrInts(List<int[]> chars) {
+		int[][] arr = new int[chars.size()][];
+		for (int i = 0; i < chars.size(); i++) {
+			arr[i] = chars.get(i);
+		}
+		return arr;
+	}
+
+	private static int searchWord(char[] chars) {
+		if (chars.length < WORD.length) {
+			return 0;
+		}
+
+		int sum = 0;
+		for (int i = 0; i < chars.length; i++) {
+			if (WORD[0] == chars[i]) {
+				int cur = 1;
+				for (int j = 1; j < WORD.length; j++) {
+					if (i + j < chars.length && WORD[j] == chars[i + j]) {
+						cur++;
+					} else {
+						break;
+					}
+				}
+				if (cur == WORD.length) {
+					sum++;
+				}
+				cur = 1;
+				for (int j = 1; j < WORD.length; j++) {
+					if (i - j >= 0 && WORD[j] == chars[i - j]) {
+						cur++;
+					} else {
+						break;
+					}
+				}
+
+				if (cur == WORD.length) {
+					sum++;
+				}
+			}
+		}
+
+		return sum;
+	}
+
+
 
 	@Override
 	public RiddleSolver<Integer> getSecondRiddleSolver() {
 		return new RiddleSolver<>() {
 			@Override
 			public Integer getSolution() {
-				return null;
+				return 9;
 			}
 
 			@Override
 			public Integer solveRiddle() {
-				return null;
+				AdvancedList<char[]> strings = splitLinesAndConvert(String::toCharArray);
+				int sum = 0;
+
+				Set<int[]> leftOccurrences = new HashSet<>();
+				for (int i = 0; i < strings.size(); i++) {
+					int j = 0;
+					List<Character> chars = new ArrayList<>();
+					List<int[]> locations = new ArrayList<>();
+
+					for (int di = 0; di < strings.size() - i; di++) {
+						chars.add(strings.get(i + di)[j + di]);
+						locations.add(new int[]{i + di, j + di});
+					}
+
+					leftOccurrences.addAll(searchWordMas(toArr(chars), toArrInts(locations)));
+				}
+
+				for (int j = 1; j < strings.get(0).length; j++) {
+					int i = 0;
+					List<Character> chars = new ArrayList<>();
+					List<int[]> locations = new ArrayList<>();
+
+					for (int di = 0; di < strings.get(0).length - j; di++) {
+						chars.add(strings.get(i + di)[j + di]);
+						locations.add(new int[]{i + di, j + di});
+					}
+
+					leftOccurrences.addAll(searchWordMas(toArr(chars), toArrInts(locations)));
+				}
+
+				Set<int[]> rightOccurrences = new HashSet<>();
+
+				for (int i = strings.size() - 1; i >= 0; i--) {
+					List<Character> chars = new ArrayList<>();
+					List<int[]> locations = new ArrayList<>();
+
+					for (int di = 0; di <= i; di++) {
+						chars.add(strings.get(i - di)[di]);
+						locations.add(new int[]{i - di, di});
+					}
+
+					rightOccurrences.addAll(searchWordMas(toArr(chars), toArrInts(locations)));
+				}
+
+				for (int j = 1; j < strings.get(0).length; j++) {
+					List<Character> chars = new ArrayList<>();
+					List<int[]> locations = new ArrayList<>();
+
+					for (int di = 0; di < strings.get(0).length - j; di++) {
+						chars.add(strings.get(strings.size() - 1 - di)[j + di]);
+						locations.add(new int[]{strings.size() - 1 - di, j + di});
+					}
+
+					rightOccurrences.addAll(searchWordMas(toArr(chars), toArrInts(locations)));
+				}
+
+				leftOccurrences.removeIf(ints -> rightOccurrences.stream().noneMatch(ints1 -> ints1[0] == ints[0] && ints1[1] == ints[1]));
+
+				return leftOccurrences.size();
 			}
 		};
 	}
+
+	private static final char[] WORD_MAS = new char[]{'M', 'A', 'S'};
+
+	private static Set<int[]> searchWordMas(char[] chars, int[][] locations) {
+		if (chars.length != locations.length) {
+			throw new IllegalArgumentException("chars and locations must have the same length");
+		}
+		if (chars.length < WORD_MAS.length) {
+			return Set.of();
+		}
+
+		Set<int[]> sum = new HashSet<>();
+		for (int i = 0; i < chars.length; i++) {
+			if (WORD_MAS[0] == chars[i]) {
+				int cur = 1;
+				for (int j = 1; j < WORD_MAS.length; j++) {
+					if (i + j < chars.length && WORD_MAS[j] == chars[i + j]) {
+						cur++;
+					} else {
+						break;
+					}
+				}
+				if (cur == WORD_MAS.length) {
+					sum.add(locations[i + 1]);
+				}
+				cur = 1;
+				for (int j = 1; j < WORD_MAS.length; j++) {
+					if (i - j >= 0 && WORD_MAS[j] == chars[i - j]) {
+						cur++;
+					} else {
+						break;
+					}
+				}
+
+				if (cur == WORD_MAS.length) {
+					sum.add(locations[i - 1]);
+				}
+			}
+		}
+
+		return sum;
+	}
+
 }
 
 
